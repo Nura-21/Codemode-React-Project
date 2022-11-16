9d1d9c82dae4b4a17fe26242fca9f0ae29136bee
Raiymbek Baktybayev <raiymbek.baktybayev@chocotravel.com>
2022-11-16 23:57:10 +0600

    add style to active link in header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "react-router-dom";
+import { links } from './data';
+
+import Logo from "../../assets/landing/logo.svg";
+
+export const Header = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  return (
+    <header className="header main-gradient">
+      <section className="header-section">
+        <Link to="/">
+          <img
+            className="header-logo"
+            src={Logo}
+            alt="Logo"
+            draggable="false"
+          />
+        </Link>
+
+        <nav className="header-nav">
+          {links.map((link, ind) => 
+          <Link key={ind} className={`header-nav-link ${pathname === link.pathname ? "router-link-exact-active" : ""}`} 
+            to={link.pathname} onClick={() => setActiveLink(ind)}>
+            {link.name}
+          </Link>)
+          }
+        </nav>
+      </section>
+    </header>
+  );
+};

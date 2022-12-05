c4d0a4560526ba24e2e274e091664a6b2669c577
Raiymbek Baktybayev <raiymbek.baktybayev@chocotravel.com>
2022-12-05 22:28:34 +0600

    add courses query

--- src/pages/Course/index.jsx
+++ src/pages/Course/index.jsx
@@ -1,18 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "../../api";
 import { CourseCard } from "./CourseCard";
-import { activeCourseList, futureCourseList } from "./data";
 
 export const Course = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [courses, setCourses] = useState();
+  
+  const fetchCourses = async () => {
+    api.getCourses()
+      .then(data => {
+        setCourses(data);
+      })
+      .catch(e => {
+        console.log(e);
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
   return (
     <section className="page course animate slide">
       <h1 className="course-title">Доступные курсы</h1>
       <section className="course-list">
-        {activeCourseList.map((course, ind) => (
-          <CourseCard key={ind} course={course} />
-        ))}
-      </section>
-      <h1 className="course-title">Coming soon...</h1>
-      <section className="course-list">
-        {futureCourseList.map((course, ind) => (
+        {courses.map((course, ind) => (
           <CourseCard key={ind} course={course} />
         ))}
       </section>

d2507e48c891c01346054a749b36d29d4ae9db30
Nura-21 <n_turdalin@kbtu.kz>
2022-12-05 22:37:38 +0600

    Courses Api

--- src/pages/Course/index.jsx
+++ src/pages/Course/index.jsx
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 import api from "../../api";
 import { CourseCard } from "./CourseCard";
-import { Loader } from  "../../components/Loader";
+import { Loader } from "../../components/Loader";
 
 export const Course = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
   const [courses, setCourses] = useState();
-  
-  const fetchCourses = async () => {
-    api.getCourses()
-      .then(data => {
-        setCourses(data);
-      })
-      .catch(e => {
-        console.log(e);
+
+  const initPage = async () => {
+    setIsLoading(true);
+    try {
+      const courses = await api.getCourses();
+      if (courses) {
+        setCourses(courses);
+      } else {
         setIsError(true);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }
+      }
+    } catch (err) {
+      setIsError(true);
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    fetchCourses();
+    initPage();
   }, []);
 
   if (isLoading) {
@@ -39,9 +41,19 @@ export const Course = () => {
     <section className="page course animate slide">
       <h1 className="course-title">Доступные курсы</h1>
       <section className="course-list">
-        {courses.map((course, ind) => (
-          <CourseCard key={ind} course={course} />
-        ))}
+        {courses
+          .filter((course) => course.isActive)
+          .map((course, ind) => (
+            <CourseCard key={ind} course={course} />
+          ))}
+      </section>
+      <h1 className="course-title">Скоро ...</h1>
+      <section className="course-list">
+        {courses
+          .filter((course) => !course.isActive)
+          .map((course, ind) => (
+            <CourseCard key={ind} course={course} />
+          ))}
       </section>
     </section>
   );
